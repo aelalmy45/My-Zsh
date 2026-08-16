a515176f6137d68f59a9589c23c2d84b56b34190
aelalmy45 <aelalmy45>
2026-08-16 19:17:09 +0200

    dev

--- banner.zsh
+++ banner.zsh
@@ -2,8 +2,18 @@
 # Terminal Banner
 # =========================================================
 
+hex_color() {
+  local hex="${1#\#}"
+
+  local r=$((16#${hex[1,2]}))
+  local g=$((16#${hex[3,4]}))
+  local b=$((16#${hex[5,6]}))
+
+  printf '\033[38;2;%d;%d;%dm' "$r" "$g" "$b"
+}
+
 banner() {
-  printf '\033[38;2;0;215;255m'
+  printf '%s' "$(hex_color '#156AFF')"
   command cat "$HOME/.config/zsh/banner"
   printf '\033[0m'
 }
